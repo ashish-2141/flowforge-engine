e6588d32b584450d72559297096d27daa6888071
ashish-2141 <asamal816@gmail.com>
2026-09-14 06:52:07 +0530

    build: add flowforge spring boot app

---
+++ flowforge-engine/src/main/java/com/ashish/flowforge/engine/FlowForgeApplication.java
@@ -0,0 +1,11 @@
+package com.ashish.flowforge.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowForgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowForgeApplication.class, args);
+    }
+}
